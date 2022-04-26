53ef09fcbebd8a6bb7d2ed0d539b8024d4d7e690
Igor Podgorniy <ipod-92@mail.ru>
2022-04-27 00:06:52 +0300

    Добавить элементы на страницу из массива. Добавить возможность лайкать фотографии

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
 const btnEdit = document.querySelector('.profile__edit-button');
 const form = document.querySelector('.popup__container');
 const btnClose = document.querySelector('.popup__close');
@@ -6,6 +33,17 @@ const nameProfile = document.querySelector('.profile__title');
 const descProfile = document.querySelector('.profile__subtitle');
 const popupNameProfile = document.querySelector('#nameProfile');
 const popupDescProfile = document.querySelector('#descProfile');
+const elementTemplate = document.querySelector('#element').content;
+const elementParent = document.querySelector('.elements__items');
+
+// Добавление карточек из массива на страницу
+initialCards.forEach(item => {
+  const elementItem = elementTemplate.querySelector('.elements__item').cloneNode(true);
+  elementItem.querySelector('.elements__image').src = item.link;
+  elementItem.querySelector('.elements__image').alt = item.name;
+  elementItem.querySelector('.elements__title').textContent = item.name;
+  elementParent.append(elementItem);
+})
 
 function popupClose() {
   popup.classList.remove('popup_opened');
@@ -28,4 +66,8 @@ btnClose.addEventListener('click', () => {
   popupClose();
 })
 
-form.addEventListener('submit', formSubmitHandler)
+form.addEventListener('submit', formSubmitHandler);
+
+elementParent.addEventListener('click', (evt) => {
+  evt.target.classList.toggle('elements__heart_active');
+})
