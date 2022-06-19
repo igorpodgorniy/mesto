274e7e8e342f948b2c9120dccb283c8f10e48237
Igor Podgorniy <ipod-92@mail.ru>
2022-06-19 19:08:26 +0300

    Реализовать класс UserInfo

--- scripts/components/UserInfo.js
+++ scripts/components/UserInfo.js
@@ -1,14 +1,17 @@
 export class UserInfo {
   constructor({selectorNameUser, selectorAboutUser}) {
-    this._selectorNameUser = selectorNameUser;
-    this._selectorAboutUser = selectorAboutUser;
+    this._nameUser = document.querySelector(selectorNameUser).textContent;
+    this._aboutUser = document.querySelector(selectorAboutUser).textContent;
   }
 
   getUserInfo() {
-    
+    const name = this._nameUser;
+    const about = this._aboutUser;
+    return { name, about };
   }
 
-  setUserInfo() {
-
+  setUserInfo(name, about) {
+    this._nameUser = name;
+    this._aboutUser = about;
   }
 }
