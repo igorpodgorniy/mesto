61a04b19750df9fb63196ce795f3dcb8ac33e4a6
Igor Podgorniy <ipod-92@mail.ru>
2022-06-21 09:41:37 +0300

    Перенести функционал открытия попапа с формой в отдельный класс

--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -2,19 +2,33 @@ import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
   constructor(selector, callbackSubmit) {
-    this._selector = selector;
+    super(selector);
+    this._popup = document.querySelector(selector);
+    this._form = this._popup.querySelector('form');
     this._callbackSubmit = callbackSubmit;
   }
 
   _getInputValues() {
-    
+    const inputList = this._popup.querySelectorAll('.popup__input');
+    const valueList = [];
+    inputList.forEach(item => {
+      valueList.push(item);
+    });
+    return inputList;
   }
 
-  setEventListeners() {
-
+  setEventListeners = (evt) => {
+    this._form.addEventListener('submit', this._callbackSubmit);
+    this._handlePopupClose(evt);
+    this._buttonClose.addEventListener('click', () => {
+      this.close();
+    });
   }
 
   close() {
-
+    this._popup.classList.remove('popup_opened');
+    this._popup.removeEventListener('mousedown', this.setEventListeners);
+    document.removeEventListener('keydown', this._handleEscClose);
+    this._form.reset();
   }
 }
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,8 +1,8 @@
 import '../../pages/index.css';
 
 import { Section } from "../components/Section.js";
-import { Popup } from "../components/Popup.js";
 import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
 import { FormValidator } from "../components/FormValidator.js";
 import { Card } from "../components/Card.js";
 import { 
@@ -12,59 +12,17 @@ import {
   popupLinkPhoto,
   nameProfile,
   descProfile,
-  popupAddPhoto,
-  formAddPhoto,
   elementParent,
   initialCards,
   btnEdit,
-  popupEditProfile,
   btnAdd,
-  formEditProfile,
-  buttonCloseList,
   configFormForValidation
  } from "../utils/constants.js";
-
-// function closePopup(popupId) {
-//   popupId.classList.remove('popup_opened');
-//   popupId.removeEventListener('mousedown', handlePopupClose)
-//   document.removeEventListener('keydown', closePopupByEsc);
-// }
-
-// function closePopupByEsc(evt) {
-//     if (evt.key === 'Escape') {
-//       const openedPopup = document.querySelector('.popup_opened');
-//       closePopup(openedPopup);
-//     }
-// }
-
-// function openPopup(popupId) {
-//   popupId.classList.add('popup_opened');
-//   popupId.addEventListener('mousedown', handlePopupClose)
-//   document.addEventListener('keydown', closePopupByEsc);
-// }
-
-function handleSubmitForm() {
-  nameProfile.textContent = popupNameProfile.value;
-  descProfile.textContent = popupDescProfile.value;
-  // closePopup(popupEditProfile);
-}
 
 function createCard(name, link, selector) {
   const card = new Card(name, link, selector);
   return card.createCard();
 }
-
-function handleAddFormSubmit() {
-  renderCard(popupNamePhoto.value, popupLinkPhoto.value, '.card');
-  // closePopup(popupAddPhoto);
-  formAddPhoto.reset();
-}
-
-// function handlePopupClose(evt) {
-//   if (evt.target.classList.contains('popup_opened')) {
-//     closePopup(evt.target);
-//   }
-// }
 
 function renderCard(name, link, selector) {
   return elementParent.prepend(createCard(name, link, selector));
@@ -89,23 +47,18 @@ const cardList = new Section({
 btnEdit.addEventListener('click', () => {
   popupNameProfile.value = nameProfile.textContent;
   popupDescProfile.value = descProfile.textContent;
-  // openPopup(popupEditProfile);
-})
-
-btnAdd.addEventListener('click', () => {
-  // openPopup(popupAddPhoto);
-})
-
-buttonCloseList.forEach(btnClose => {
-  btnClose.addEventListener('click', (evt) => {
-    // closePopup(evt.target.closest('.popup'));
-  })
+  const popupEditProfile = new PopupWithForm(
+    '#popupEditProfile',
+    () => {
+      nameProfile.textContent = popupNameProfile.value;
+      descProfile.textContent = popupDescProfile.value;
+      popupEditProfile.close();
+    }
+  );
+  popupEditProfile.open();
 })
 
 cardList.rendererAll();
-
-formEditProfile.addEventListener('submit', handleSubmitForm);
-formAddPhoto.addEventListener('submit', handleAddFormSubmit);
 
 // Включение валидации форм
 const photoForm = new FormValidator({
@@ -121,4 +74,13 @@ const profileForm = new FormValidator({
 photoForm.enableValidation();
 profileForm.enableValidation();
 
-// export { openPopup }
+btnAdd.addEventListener('click', () => {
+  const popupAddPhoto = new PopupWithForm(
+    '#popupAddPhoto',
+    () => {
+      renderCard(popupNamePhoto.value, popupLinkPhoto.value, '.card');
+      popupAddPhoto.close();
+    }
+  );
+  popupAddPhoto.open();
+})
